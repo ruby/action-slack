ba8878b73fdf3cbd212998959ab32af31a47f797
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-19 02:48:04 +0900

    Implement action-slack

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import * as core from '@actions/core';
+import { IncomingWebhook } from '@slack/webhook';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    if (process.env.SLACK_WEBHOOK_URL === undefined) {
+      throw new Error('SLACK_WEBHOOK_URL is not set');
+    }
+    const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+    const payload = JSON.parse(core.getInput('payload'));
+
+    await webhook.send(payload);
   } catch (error) {
     core.setFailed(error.message);
   }
